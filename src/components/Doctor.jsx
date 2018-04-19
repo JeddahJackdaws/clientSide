2a30bd137d69441d5c182cd4d721802c2e7efc64
Bassam <32364350+Bxssam@users.noreply.github.com>
2018-04-19 05:57:49 +0300

    fixed the post

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -16,12 +16,14 @@ import {
   href
 } from 'react-bootstrap';
 import Nbar from './Nbar.jsx';
+import Reviews from './reviews.jsx';
 import Footer from './Footer.jsx';
 import './css/result2.css';
 import x from '../images/x.png';
 
 export default class Doctor extends Component {
   constructor(props) {
+    var test;
     super(props);
     this.state = {
       error: null,
@@ -34,8 +36,8 @@ export default class Doctor extends Component {
     const {match: {
         params
       }} = this.props;
-    var test = params.id;
-    fetch('https://betterdoc.herokuapp.com/doctors/id/' + test)
+    this.test = params.id;
+    fetch('https://betterdoc.herokuapp.com/doctors/id/' + this.test)
       .then(res => res.json())
       .then((result) => {
         this.setState({isLoaded: true, Doctors: result});
@@ -55,51 +57,54 @@ export default class Doctor extends Component {
       return (
         <div>
           <Nbar/>
-          {Doctors.map(Doctor => (
           <div className="container">
+            {Doctors.map(doctor => (
+              <div>
+                <div className="row mt-5">
+                  <h1 className="text ">Name: {doctor.name}</h1>
+                </div>
+                <h1 className="text ">city: {doctor.city}</h1>
+
+                <Image src={x} circle className="img"/>
+                <Reviews id={this.test}/>
+              </div>
+            ))}
             <div className="row mt-5">
-              <h1 className="text ">Name: {Doctor.name}</h1>
+              <div className="col-md-3"></div>
+              <div className="col-md-6">
+                <div className="panel panel-info">
+                  <div className="panel-body">
+                    <form
+                      className="form-inline"
+                      action="https://betterdoc.herokuapp.com/comments/new"
+                      method="post">
+                      <input type="hidden" name="reviewerId" value="15"/>
+                      <input type="hidden" name="medicalId" value={this.test}/>
+                      <textarea
+                        name="reviewText"
+                        placeholder="Write your comment here!"
+                        className="pb-cmnt-textarea"></textarea>
+
+                      <div className="input-group mb-3">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Your username"
+                          name="reviewerName"
+                          aria-describedby="basic-addon2"/>
+                        <div className="input-group-append">
+                          <button className="btn btn-outline-secondary" type="submit">share</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-              <h1 className="text ">Specialty: {Doctor.speciality}</h1>
-            
-          <Image src={x} circle className="img"/>
-          <div id="list-example" class="list-group">
-            <ul className="low1">
-              <li>
-                <a class="list-group-item list-group-item-action">Review
-                </a>
-              </li>
-            </ul>
           </div>
-          
-          </div>
-        ))}
-        <Footer/>
+          <Footer/>
         </div>
       );
     }
   }
 }
-
-
-{/* <div>
-<Nbar/>
-<div className="container">
-  <div className="row mt-5">
-    {Doctors.map(Doctor => (
-      <h3 className="text ">Name: {Doctor.name}</h3> 
-        <h3 className="text "> Specialty : {Doctor.speciality} </h3>
-          <Image src={x} circle className="img"/> 
-            <div id="list-example" class="list-group"></div> 
-              <div className = "row mt-5"> 
-                <ul className="low1">
-                  <li>
-                    <a class="list-group-item list-group-item-action">Review</a>
-                  </li>
-                </ul> 
-              </div>
-    ))}
-  </div>
-</div>
-< Footer/>
-</div> */}
